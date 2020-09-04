894c2645e5f4fcb9e5f82596de100540261809eb
ksk001100 <hm.pudding0715@gmail.com>
2020-09-04 10:10:20 +0900

    first commit

---
+++ chroot.bash
@@ -0,0 +1,18 @@
+# locale setup
+sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i -e 's/#ja_JP.UTF-8 UTF-8/ja_JP.UTF-8 UTF-8/g' /etc/locale.gen
+grep -E '^(en_US|ja_JP)\.UTF\-8 UTF\-8' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# timezone setup
+tzselect
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+hwclock --systohc --utc
+
+# initramfs
+mkinitcpio -p linux-zen
+
+# boot loader setup
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch --boot-directory=/boot/efi/EFI --recheck
+grub-mkconfig -o /boot/efi/EFI/grub/grub.cfg
---
+++ setup.zsh
@@ -0,0 +1,27 @@
+# partition setup
+sgdisk -z /dev/nvme0n1
+timeout 10 dd if=/dev/zero of=/dev/nvme0n1 bs=4M
+sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI System" /dev/nvme0n1p1
+sgdisk -n 2:0:+512M -t 2:8300 -c 2:"Linux filesystem" /dev/nvme0n1p2
+sgdisk -n 3:0: -t 3:8300 -c 3:"Linux filesystem" /dev/nvme0n1p3
+
+# partition format
+mkfs.vfat -F32 /dev/nvme0n1p1
+mkfs.ext4 /dev/nvme0n1p2
+mkfs.xfs /dev/nvme0n1p3
+
+# partition mount
+mount /dev/nvme0n1p3 /mnt
+mkdir /mnt/boot
+mount /dev/nvme0n1p2 /mnt/boot
+mkdir /mnt/boot/efi
+mount /dev/nvme0n1p1 /mnt/boot/efi
+
+# base package install
+pacstrap /mnt base base-devel linux-zen linux-firmware grub dosfstools efibootmgr netctl iw wpa_supplicant networkmanager dialog xfsprogs vim
+
+# create fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# chroot
+arch-chroot /mnt /bin/bash
